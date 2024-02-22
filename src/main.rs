57be9f0f18d9540da5e25ebaabc2b70b579e23d5
Jyothis <jyothisv@gmx.com>
2024-02-22 10:07:25 -0500

    Add 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
 
@@ -11,12 +14,28 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_client(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) -> anyhow::Result<()> {
+    let mut buf = [0u8; 512];
+    let response = "+PONG\r\n";
+
+    loop {
+        let bytes_read = stream.read(&mut buf)?;
+
+        if bytes_read == 0 {
+            return Ok(());
+        }
+
+        stream.write_all(response.as_bytes())?;
     }
 }
