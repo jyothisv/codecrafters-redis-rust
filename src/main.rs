7c441b77aaaac97e8726130c81a2647b17e1d725
Jyothis <jyothisv@gmx.com>
2024-02-22 10:12:01 -0500

    Remove comments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-// Uncomment this block to pass the first stage
 use std::{
     io::{Read, Write},
     net::{TcpListener, TcpStream},
 };
 
 fn main() -> anyhow::Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
