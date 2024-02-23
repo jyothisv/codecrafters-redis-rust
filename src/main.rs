6fc700f81223a06523ef87655298ab77ceddbf27
Jyothis <jyothisv@gmx.com>
2024-02-23 18:20:51 -0500

    Add a store for set and get commands

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 mod commands;
 mod resp;
 
+use commands::CommandHandler;
 use resp::Resp;
 
 use std::{
+    collections::HashMap,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
     thread,
 };
 
 fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+    let store: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(move || handle_client(stream));
+                let store = Arc::clone(&store);
+                thread::spawn(move || handle_client(stream, store));
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -25,8 +31,12 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) -> anyhow::Result<()> {
+fn handle_client(
+    mut stream: TcpStream,
+    store: Arc<Mutex<HashMap<String, String>>>,
+) -> anyhow::Result<()> {
     let mut buf = [0u8; 512];
+    let mut command_handler = CommandHandler::new(store);
 
     loop {
         let bytes_read = stream.read(&mut buf)?;
@@ -39,7 +49,7 @@ fn handle_client(mut stream: TcpStream) -> anyhow::Result<()> {
 
         let (parsed, _) = Resp::parse_command(str)?;
 
-        let response = commands::handle_command(parsed)?;
+        let response = command_handler.handle_command(parsed)?;
 
         stream.write_all(response.as_bytes())?;
     }
