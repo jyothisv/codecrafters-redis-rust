6e85e4abaea7d035243f04e904223ca123b3af6a
Jyothis <jyothisv@gmx.com>
2024-03-11 11:14:17 -0400

    Send empty RDB file for stage 10

--- src/commands/response.rs
+++ src/commands/response.rs
@@ -6,18 +6,20 @@ pub enum Response {
     Null,
     SimpleString(String),
     BulkString(String),
+    File(String),
+    Seq(Vec<Response>),
 }
 
 impl Response {
-    pub fn serialize(&self) -> Vec<u8> {
+    pub fn serialize(&self) -> String {
         match self {
             Response::OK => "OK".as_simple_string().serialize(),
             Response::Pong => "PONG".as_simple_string().serialize(),
             Response::Null => "$-1\r\n".to_owned(),
             Response::SimpleString(s) => s.as_simple_string().serialize(),
             Response::BulkString(s) => s.as_bulk_string().serialize(),
+            Response::File(s) => s.as_bulk_string().serialize(),
+            Response::Seq(seq) => seq.iter().map(|s| s.serialize()).collect(),
         }
-        .as_bytes()
-        .to_vec()
     }
 }
